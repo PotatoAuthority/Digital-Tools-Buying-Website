9df255dd38e16d653ea6675d1a89a01a109e1dc3
PotatoAuthority <ahsanmanjurul27@gmail.com>
2026-04-17 15:36:59 +0600

    toogle button added

--- src/Components/Main section/MainSection.jsx
+++ src/Components/Main section/MainSection.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const MainSection = () => {
+    const [toggle, setToggle] = useState('products');
+
     return (
         <div className='w-[90%] md:w-70% md:max-w-[1200px] mx-auto my-15 md:my-30'>
             
@@ -9,6 +11,18 @@ const MainSection = () => {
                 <div>
                     <p className='text-[16px] leading-5 text-[#627382] mb-4'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity.</p>
                 </div>
+            </div>
+            <div className='flex justify-center mb-10'>
+                <button onClick={() => setToggle('products')} 
+                    className={`${toggle==="products"? 'btn-primary' : 'btn-outline'} btn  btn-primary rounded-r-none rounded-l-full`}>
+                Products</button>
+                <button onClick={() => setToggle('cart')} 
+                    className={`${toggle==="cart"? 'btn-primary' : 'btn-outline'} btn  btn-primary rounded-l-none rounded-r-full`}>
+                Cart (0)</button>
+            </div>
+
+            <div className="products">
+
             </div>
             <p>cards</p>
         </div>
